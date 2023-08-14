195794ce1befdd18854d6f7c2c04aedeed3df3c3
KonradBartu <33003781+KonradBartu@users.noreply.github.com>
2023-08-14 10:25:20 +0100

    Add files via upload

--- lotto mobile.js
+++ lotto mobile.js
@@ -37,7 +37,7 @@ function timeoutsWinAlert() {
   setTimeout(() => {
     document.querySelector("h1").innerText = "Wprowadź swoje numery";
   }, 2000);
-  window.navigator.vibrate(60);
+  window.navigator.vibrate(30);
 }
 
 
@@ -163,7 +163,7 @@ f6.addEventListener("change", function () {
 // Button click. Vibrate on click
 document.querySelector(".btn").addEventListener("click", function () {
   // window.navigator?
-  navigator.vibrate(60);
+  navigator.vibrate(30);
 
 if (
     typeof numberArray[0] === "number" &&
